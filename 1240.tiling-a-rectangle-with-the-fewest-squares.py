0965e424b7f3eecbc5d257e267672cae743942e3
Jonny <jpalactor@gmail.com>
2022-08-16 03:07:17 -0600

    Few tests

---
+++ 1240.tiling-a-rectangle-with-the-fewest-squares.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1240 lang=python3
+#
+# [1240] Tiling a Rectangle with the Fewest Squares
+#
+
+# @lc code=start
+class Solution:
+    def tilingRectangle(self, n: int, m: int) -> int:
+        
+# @lc code=end
+
